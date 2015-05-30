9f70b4774c95d41e082e81e763e057f61319091e
XoveR1 <vicheslav.p@tut.by>
2015-05-30 20:24:15 +0300

    Fix mobile aside nav + add dropdowns

--- js/ui/aside.js
+++ js/ui/aside.js
@@ -1,5 +1,11 @@
 function AsideNav(){
+    const STATE_CLOSED = 'closed';
+    const STATE_ANIMATION = 'animation';
+    const STATE_OPENED = 'opened';
+    const MOBILE_WIDTH = 752;
+
     var self = this;
+    this.asideState = STATE_CLOSED;
     this.$asideBlock = $('.products-list aside');
     this.$mainWrapper = $('#main-wrapper, footer');
     this.$body = $('body');
@@ -8,44 +14,65 @@ function AsideNav(){
     this.resetStyles = function(){
         self.$mainWrapper.removeAttr('style');
         self.$asideBlock.removeAttr('style');
+        self.$body.css('width', 'auto');
+        self.asideState = STATE_CLOSED;
     };
 
     this.removeSelecttion = function(){
         document.getSelection().removeAllRanges();
     };
 
+    this.isMobileScreen = function(){
+        return self.$mainWrapper.width() <= MOBILE_WIDTH;
+    };
+
     $(window).on("swiperight", function(e) {
+        if(!self.isMobileScreen()){return}
         e.preventDefault();
         self.removeSelecttion();
-        self.$mainWrapper.css('width', this.$mainWrapper.width()).transition({
-            marginLeft: '300px',
-            duration: 700
-        });
+        if(self.asideState == STATE_CLOSED) {
+            var windowWidth = this.$mainWrapper.width();
+            self.asideState = STATE_ANIMATION;
 
-        self.$asideBlock.transition({
-            'left': '0',
-            duration: 700
-        });
+            self.$body.css('width', windowWidth);
+            self.$mainWrapper.css('width', windowWidth).transition({
+                marginLeft: '300px',
+                duration: 700
+            }).queue(function(){
+                $(this).dequeue();
+                self.asideState = STATE_OPENED;
+            });
+
+            self.$asideBlock.transition({
+                'left': '0',
+                duration: 700
+            });
+        }
     });
 
     $(window).on("swipeleft", function(e) {
+        if(!self.isMobileScreen()){return}
         e.preventDefault();
         self.removeSelecttion();
-        self.$mainWrapper.transition({
-            marginLeft: 0,
-            duration: 700
-        }).queue(function(){
-            $(this).css('width', 'auto').dequeue();
-            self.resetStyles();
-        });
+        if(self.asideState == STATE_OPENED) {
+            self.asideState = STATE_ANIMATION;
+            self.$mainWrapper.transition({
+                marginLeft: 0,
+                duration: 700
+            }).queue(function () {
+                $(this).css('width', 'auto').dequeue();
+                self.resetStyles();
+            });
 
-        self.$asideBlock.transition({
-            'left': '-300px',
-            duration: 700
-        });
+            self.$asideBlock.transition({
+                'left': '-300px',
+                duration: 700
+            });
+        }
     });
 
     $(window).on("resize", function(){
+        if(!self.isMobileScreen()){return}
         self.resetStyles();
     });
 };
