04dded418645917b6ccc65b4b0a37ca0556aa007
slava.poddubsky <vyacheslav.poddubsky@itechart-group.com>
2015-06-24 11:02:15 +0300

    Fix details page for mobile

---
+++ js/ui/tabs.js
@@ -0,0 +1,11 @@
+(function($) {
+    $.fn.customTabs = function (options) {
+        const MOBILE_WIDTH = 752;
+
+        options = options || {};
+    }
+})(jQuery);
+
+$(document).ready(function(){
+    $('.product-details .menu').customTabs();
+});
