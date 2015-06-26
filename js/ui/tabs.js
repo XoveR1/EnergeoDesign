7091a58f89335faeefa87607d38815a7c64d3eec
slava.poddubsky <vyacheslav.poddubsky@itechart-group.com>
2015-06-26 09:44:09 +0300

    Implement tabs

--- js/ui/tabs.js
+++ js/ui/tabs.js
@@ -3,6 +3,26 @@
         const MOBILE_WIDTH = 752;
 
         options = options || {};
+        var tabMenuSelector = this.selector;
+        var tabBtnSelector = options.tabBtnSelector || 'a'
+        var tabElemToActivate = options.tabElemToActivate || 'li'
+        var activeClass = options.activeClass || 'active'
+
+        $(document).on('click', tabMenuSelector + ' ' + tabBtnSelector, function(e){
+            e.preventDefault()
+            var $tabBtn = $(this),
+                $elemToActivate = $tabBtn.closest(tabElemToActivate),
+                linkedTabID = $tabBtn.data('tab'),
+                $linkedTab = $(linkedTabID);
+
+            $elemToActivate.addClass(activeClass)
+                .siblings()
+                .removeClass(activeClass)
+
+            $linkedTab.addClass(activeClass)
+                .siblings()
+                .removeClass(activeClass)
+        })
     }
 })(jQuery);
 
